587b4179156fcaad6ddfb16843f7afe3122d7ac3
Markus Fischer <fischer@mideasdesign.de>
2025-03-06 18:35:59 +0100

    add noteInput function

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-let notes = ['Freitag', 'Samstag', 'Sonntag'];
+let notes = [];
 function rendererNotes(){
 let contentRef = document.getElementById('content')
 contentRef.innerHTML = notes;
+}
+function addNote(){
+let noteInputRef = document.getElementById('input_note');
+let noteInput = noteInputRef.value;
+notes.push(noteInput);
+
+rendererNotes();
+noteInputRef.value ="";
 }
