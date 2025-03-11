400a8996ee937b156eb21fac1232c72cdbb36b26
Markus Fischer <fischer@mideasdesign.de>
2025-03-11 10:26:53 +0100

    fixed trash issue update Notes

--- main.js
+++ main.js
@@ -1,90 +1,92 @@
+
 let notes = [];
 let trash = [];
 let notesTitles = [];
 let notesTitlesTrash = [];
 
-function renderAll(){
-    renderNotes();
-    renderTrashNotes();
-/*     renderArchiveNotesTitle(); */
-}
+function init(){
+    getFromLocalStorage();
+        renderNotes();
+    }
 
 function renderNotes() {
-  getFromLocalStorage();
-  let notesRef = document.getElementById("notes-list");
-  let notesTitleRef = document.getElementById("notes-list");
-  notesRef.innerHTML = "";
-  notesTitleRef.innerHTML = "";
-  for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-    notesRef.innerHTML += getNotesTemplate(indexNote);
-  }
+    let notesRef = document.getElementById('notes-container');
+    let notesTitleRef = document.getElementById('notes-container');
+   notesRef.innerHTML = "";
+   notesTitleRef.innerHTML = "";
+    for (let indexNote = 0; indexNote < notes.length; indexNote++) {
+        notesRef.innerHTML += getNotesTemplate(indexNote);
+    }
 }
 
 function renderTrashNotes() {
-  let trashNotesRef = document.getElementById("notes-trash");
-  trashNotesRef.innerHTML = "";
-  for (
-    let indexTrashNote = 0;
-    indexTrashNote < trash.length;
-    indexTrashNote++
-  ) {
-    trashNotesRef.innerHTML += getTrashNotesTemplate(indexTrashNote);
-  }
+    let trashNotesRef = document.getElementById('notes-trash');
+   trashNotesRef.innerHTML = "";
+    for (let indexTrashNote = 0; indexTrashNote < trash.length; indexTrashNote++) {
+        trashNotesRef.innerHTML += getTrashNotesTemplate(indexTrashNote);
+    }
 }
-
 function renderTrashNotesTitles() {
-  let trashNotesTitlesRef = document.getElementById("notes-trash");
-  trashNotesTitlesRef.innerHTML = "";
-  for (
-    let indexTrashNoteTitle = 0;
-    indexTrashNoteTitle < trash.length;
-    indexTrashNoteTitle++
-  ) {
-    trashNotesTitlesRef.innerHTML += getTrashNotesTemplate(indexTrashNoteTitle);
-  }
+    let trashNotesTitlesRef = document.getElementById('notes-trash');
+    trashNotesTitlesRef.innerHTML = "";
+    for (let indexTrashNoteTitle = 0; indexTrashNoteTitle < trash.length; indexTrashNoteTitle++) {
+        trashNotesTitlesRef.innerHTML += getTrashNotesTemplate(indexTrashNoteTitle);
+    }
 }
 
 function addNote(indexNote) {
-  let noteContentRef = document.getElementById("note-content");
-  let noteTitleRef = document.getElementById("note-title");
-  let noteTitleInput = noteTitleRef.value;
-  let noteInput = noteContentRef.value;
-  notesTitles.push(noteTitleInput);
-  notes.push(noteInput);
-  saveToLocalStorage();
-  renderNotes();
-  noteTitleRef.value = "";
-  noteContentRef.value = "";
+    let noteContentRef = document.getElementById('note-content');
+    let noteTitleRef = document.getElementById('note-title');
+    let noteTitleInput = noteTitleRef.value;    
+    let noteInput = noteContentRef.value;  
+    notesTitles.push(noteTitleInput);
+    notes.push(noteInput);
+    saveToLocalStorage();
+    renderNotes();
+    noteTitleRef.value = "";
+    noteContentRef.value = "";
 }
 
 function saveToLocalStorage() {
-  localStorage.setItem("notes", JSON.stringify(notes));
-  localStorage.setItem("notesTitles", JSON.stringify(notesTitles));
-}
+    localStorage.setItem("notes", JSON.stringify(notes)); 
+    localStorage.setItem("notesTitles", JSON.stringify(notesTitles)); 
+   }
 
-function getFromLocalStorage() {
-  let notesList = JSON.parse(localStorage.getItem("notes"));
-    notes = notesList;
+   function getFromLocalStorage() {
+    let notesList = JSON.parse(localStorage.getItem("notes"));
+    let notesListTitles = JSON.parse(localStorage.getItem("notesTitles"));
+    if (notesList == null) {
+        document.getElementById('notes-list').innerHTML += "I have changed!";
+    }
+    else{
+        notes = notesList;
+        notesTitles = notesListTitles;
+    }
 }
 
 function emtyTrash(indexTrashNote) {
-  trash.splice(indexTrashNote, 1);
-  localStorage.removeItem("notes");
-  localStorage.removeItem("notesTitles");
-  renderAll();
+    trash.splice(indexTrashNote, 1);
+    localStorage.removeItem("notes");
+    localStorage.removeItem("notesTitles");
+    renderTrashNotes();
+    renderNotes();
 }
 
 function addToTrash(indexNote) {
-  let trashNote = notes.splice(indexNote, 1);
-  trash.push(trashNote[0]);
-  let trashNoteTitles = notesTitles.splice(indexNote, 1);
-  notesTitlesTrash.push(trashNoteTitles[0]);
-  let toggleRef = document.getElementById("my-modal");
-  toggleRef.classList.toggle("close");
-renderAll();
+    let trashNote = notes.splice(indexNote, 1);
+    trash.push(trashNote[0]);
+    let trashNoteTitles = notesTitles.splice(indexNote, 1);
+    notesTitlesTrash.push(trashNoteTitles[0]);
+    let toggleRef = document.getElementById('my-modal')
+    toggleRef.classList.toggle('close');
+    renderTrashNotes();
+    renderTrashNotesTitles();
+    renderNotes();
 }
 
-function modalOverlay(event) {
-  let toggleRef = document.getElementById("my-modal");
-  toggleRef.classList.toggle("close");
+
+
+function modalOverlay(event){
+    let toggleRef = document.getElementById('my-modal')
+    toggleRef.classList.toggle('close');
 }
--- scripts_notes.js
+++ scripts_notes.js
@@ -1,14 +1,81 @@
-let notes = ['banana', 'Erdbeere', 'apfel'];
+let notes = ['ham', 'Egg'];
+let trash = [];
+let notesTitles = ['Titel1', 'Titel2'];
+let notesTitlesTrash = [];
 
+function init(){
+getFromLocalStorage();
+    renderNotes();
+}
 
 function renderNotes() {
     let notesRef = document.getElementById('notes-list');
-    notesRef.innerHTML = "";
+   notesRef.innerHTML = "";
     for (let indexNote = 0; indexNote < notes.length; indexNote++) {
-        notesRef.innerHTML += getNotesTemplate(note); 
-        
+        notesRef.innerHTML += getNotesTemplate(indexNote);
     }
 }
-function getNotesTemplate(note) {
-    return `<p>+ ${note} </p>`
+
+function renderTrashNotes() {
+    let trashNotesRef = document.getElementById('notes-trash');
+   trashNotesRef.innerHTML = "";
+    for (let indexTrashNote = 0; indexTrashNote < trash.length; indexTrashNote++) {
+        trashNotesRef.innerHTML += getTrashNotesTemplate(indexTrashNote);
+    }
 }
+function renderTrashNotesTitle() {
+    let trashNotesTitlesRef = document.getElementById('notes-trash');
+    trashNotesTitlesRef.innerHTML = "";
+    for (let indexTrashNoteTitle = 0; indexTrashNoteTitle < trash.length; indexTrashNoteTitle++) {
+        trashNotesTitlesRef.innerHTML += getTrashNotesTemplate(indexTrashNoteTitle);
+    }
+}
+
+
+function addNote(indexNote) {
+    let noteContentRef = document.getElementById('input-note');
+    let noteTitleRef = document.getElementById('input-note-title');
+    let noteTitleInput = noteTitleRef.value;    
+    let noteInput = noteContentRef.value;  
+    notesTitles.push(noteTitleInput);
+    notes.push(noteInput);
+    saveToLocalStorage();
+    renderNotes();
+    noteTitleRef.value = "";
+    noteContentRef.value = "";
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem("notes", JSON.stringify(notes));
+    localStorage.setItem("notesTitles", JSON.stringify(notesTitles));
+  }
+  
+  function getFromLocalStorage() {
+    let notesList = JSON.parse(localStorage.getItem("notes"));
+    let notesListTitle = JSON.parse(localStorage.getItem("notesTitles"));
+      notes = notesList;
+      notesListTitle = notesTitles;
+  }
+
+function emtyTrash(indexTrashNote) {
+    trash.splice(indexTrashNote, 1);
+    renderTrashNotes();
+}
+
+function addToTrash(indexNote) {
+    let trashNote = notes.splice(indexNote, 1);
+    trash.push(trashNote[0]);
+    let trashNoteTitles = notesTitles.splice(indexNote, 1);
+    notesTitlesTrash.push(trashNoteTitles[0]);
+    
+    renderNotes();
+    renderTrashNotes();
+}
+
+function getNotesTemplate(indexNote) {
+    return `<p>+ ${notesTitles[indexNote]} -  ${notes[indexNote]} <button onclick="addToTrash(${indexNote})">X</button></p>`
+}
+ function getTrashNotesTemplate(indexTrashNote) {
+    return `<p>+ ${trash[indexTrashNote]} <button onclick="emtyTrash(${indexTrashNote})">emty trash</button></p>`
+}
+
